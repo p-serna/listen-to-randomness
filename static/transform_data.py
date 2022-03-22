f318882407c5578d5111413ef02e623021b0cccf
Pablo <17181462+pabloserna@users.noreply.github.com>
2022-03-23 00:24:44 +0100

    transform severl datasets to json

--- static/transform_data.py
+++ static/transform_data.py
@@ -1,17 +1,25 @@
 import numpy as np
 import json
+import os 
 
-a = np.loadtxt("data/data.dat")
-bells = a[a[:,2]==0,:]
-swells = a[a[:,2]!=0,:]
+datafiles = [f"data/{f}" for f in os.listdir("data") if f.split(".")[-1]=="dat"]
 
 labels = ["bells","swells"]
-for temp, label in zip([bells,swells],labels):
-  a = temp*1.0
-  dt = a[1:,0]-a[:-1,0]
-  a[:-1,0] = dt
-  a[-1,0] = a[-2,0]
+adata = {label:[] for label in labels}
+for fname in datafiles:
+  a = np.loadtxt(fname)
+  bells = a[a[:,2]==0,:]
+  swells = a[a[:,2]!=0,:]
 
-  b = [list(e) for e in a]
+  for temp, label in zip([bells,swells],labels):
+    a = temp*1.0
+    dt = a[1:,0]-a[:-1,0]
+    a[:-1,0] = dt
+    a[-1,0] = a[-2,0]
+
+    b = [list(e) for e in a]
+    adata[label].extend(b)
+    
+for label in labels:
   with open(f"data/data_{label}.json","w") as f:
-      json.dump(b,f)
+    json.dump(adata[label],f)
